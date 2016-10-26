c5de40d76381d7fb321782f5ad848d563fe4763e
Daniel <daniellotzsa@gmail.com>
2016-10-26 23:06:14 +0200

    added queen

---
+++ src/client/models/Queen.java
@@ -0,0 +1,14 @@
+package client.models;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * Created by Daniel on 2016/10/26.
+ */
+public class Queen extends ChessPiece
+{
+    public Queen(int hPos, int vPos) {
+        super(0, new ArrayList<>(Arrays.asList(direction.up, direction.left, direction.right, direction.down, direction.downright, direction.downleft, direction.upleft, direction.upright)), pieces.queen, hPos, vPos);
+    }
+}
