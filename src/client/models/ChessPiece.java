cc259537903c33859dbfd0770257d9a1eeea5d1d
Daniel <daniellotzsa@gmail.com>
2016-10-26 22:36:07 +0200

    Basic piece object

--- src/client/models/ChessPiece.java
+++ src/client/models/ChessPiece.java
@@ -1,5 +1,7 @@
 package client.models;
 
+import java.util.ArrayList;
+
 /**
  * Created by Daniel on 2016/10/26.
  */
@@ -11,21 +13,34 @@ public class ChessPiece
     private enum direction {
         up, down, left, right, upleft, upright, downleft, downright
     }
-    private int moveLength;
-    /**private enum pos
-    {
-        a1, a2, a3, a4, a5, a6, a7, a8,
-        b1, b2, b3, b4, b5, b6, b7, b8,
-        c1, c2, c3, c4, c5, c6, c7, c8,
-        d1, d2, d3, d4, d5, d6, d7, d8,
-        e1, e2, e3, e4, e5, e6, e7, e8,
-        f1, f2, f3, f4, f5, f6, f7, f8,
-        g1, g2, g3, g4, g5, g6, g7, g8,
-        h1, h2, h3, h4, h5, h6, h7, h8
-    }**/
-
+    private int maxLength;
+    private ArrayList<direction> posMovements[];
+    private pieces type;
     private int hPos;
     private int vPos;
+
+    public int gethPos() {
+        return hPos;
+    }
+
+    public int getvPos() {
+        return vPos;
+    }
+
+    public void setPos(int v, int h)
+    {
+        this.hPos = h;
+        this.vPos = v;
+    }
+
+    public ChessPiece(int maxLength, ArrayList<direction>[] posMovements, pieces type, int hPos, int vPos) {
+
+        this.maxLength = maxLength;
+        this.posMovements = posMovements;
+        this.type = type;
+        this.hPos = hPos;
+        this.vPos = vPos;
+    }
 
     public void moveUp(int move)
     {
@@ -70,6 +85,4 @@ public class ChessPiece
         vPos -= ver;
         hPos += hos;
     }
-
-
 }
---
+++ src/client/models/pawn.java
@@ -0,0 +1,7 @@
+package client.models;
+
+/**
+ * Created by Daniel on 2016/10/26.
+ */
+public class pawn {
+}
